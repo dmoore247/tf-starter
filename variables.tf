0c29095afa64c24aae70b151af960f4233b86509
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 18:20:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 # To setup provider for simple workspace admin
 
-variable "host" {
-  type        = string
-  description = "Workspace host url including https"
-}
+#variable "host" {
+#  type        = string
+#  description = "Workspace host url including https"
+#}
 
-variable "token" {
-  type        = string
-  description = "PAT token for the workspace. Log into workspace as admin and create a Personal Access Token under 'user settings'"
-}
+#variable "token" {
+#  type        = string
+#  description = "PAT token for the workspace. Log into workspace as admin and create a Personal Access Token under 'user settings'"
+#}
