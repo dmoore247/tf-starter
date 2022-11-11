e8a8c29a6b18be9656b3ab248a7be18b93dd6f48
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 17:09:54 -0500

    Update teamone_clusters.tf

--- teamone_clusters.tf
+++ teamone_clusters.tf
@@ -7,13 +7,19 @@ data "databricks_spark_version" "latest_lts" {
 }
 
 resource "databricks_cluster" "shared_autoscaling" {
-  cluster_name            = "Shared Autoscaling"
+  cluster_name            = "TeamOne Shared Autoscaling"
+  
   idempotency_token       = "4d3b5de0-fa6b-43c9-b358-e8f0c04f867d"
   spark_version           = data.databricks_spark_version.latest_lts.id
   node_type_id            = data.databricks_node_type.smallest.id
+  
   autotermination_minutes = 20
   autoscale {
     min_workers = 1
     max_workers = 10
+  }
+  
+  custom_tags = {
+    "Team" = "TeamOne"
   }
 }
