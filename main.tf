e284b22b03b77051247bc0a4ea2e53d8de545e52
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 11:32:42 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "databricks_mws_workspaces" "this" {
+  provider       = databricks.mws
+  account_id     = var.databricks_account_id
+  aws_region     = var.aws_region
+  workspace_name = var.workspace_name
+}
