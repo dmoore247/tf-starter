4e2761ca48ba7b209456d2f7f74d785c3e12d8bb
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 10:50:11 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "databricks_account_username" {
+  type        = string
+  description = "Databricks Account username"
+}
+
+variable "databricks_account_password" {
+  type        = string
+  description = "Databricks Account password"
+}
+
+variable "host" {
+  type        = string
+  description = "Databricks Workspace URL"
+}
