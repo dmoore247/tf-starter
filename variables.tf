88d91ba668f6add01bb114c3cb86e93ce2e11aeb
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 12:19:41 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,6 @@
-variable "databricks_account_username" {
-  type        = string
-  description = "Databricks Account username"
-}
-
-variable "databricks_account_password" {
-  type        = string
-  description = "Databricks Account password"
-}
-
-variable "databricks_account_id" {
-  type        = string
-  description = "Databricks Customer Account ID guid"
-}
+# To setup provider for simple workspace admin
 
 variable "host" {
-  type        = string
-  description = "Workspace host url including https"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "Workspace host url including https"
-}
-
-variable "workspace_name" {
   type        = string
   description = "Workspace host url including https"
 }
