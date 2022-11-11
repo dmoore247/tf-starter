6acdc17b122c5d8f2577f360b0fd02b792d17a51
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 17:21:00 -0500

    Update terraform-cloud.tf

--- terraform-cloud.tf
+++ terraform-cloud.tf
@@ -1,4 +1,7 @@
 # required by Hashicorp's Terraform Cloud. Align organization and workspaces to your setup in Terraform Cloud.
+#
+# Change organization and workspace name to match your own
+#
 
 terraform {
   cloud {
