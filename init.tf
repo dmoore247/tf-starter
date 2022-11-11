d9c9f9631fd568e642b37ec504532820ca2f417f
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 11:30:29 -0500

    Update init.tf

--- init.tf
+++ init.tf
@@ -11,15 +11,11 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
 // Initialize provider in "MWS" mode to provision the new workspace.
 // alias = "mws" instructs Databricks to connect to https://accounts.cloud.databricks.com, to create
 // a Databricks workspace that uses the E2 version of the Databricks on AWS platform.
 // See https://registry.terraform.io/providers/databricks/databricks/latest/docs#authentication
-provider "databricks_mws_workspaces" {
+provider "databricks" {
   alias    = "mws"
   host     = var.host
   username = var.databricks_account_username
