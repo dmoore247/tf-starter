7878efd2995e7ffa941769fb9e026e04f539eafc
Douglas Moore <dmoore247@users.noreply.github.com>
2022-11-11 11:47:45 -0500

    Delete main.tf

--- main.tf
+++
@@ -1,6 +0,0 @@
-resource "databricks_mws_workspaces" "this" {
-  provider       = databricks.mws
-  account_id     = var.databricks_account_id
-  aws_region     = var.aws_region
-  workspace_name = var.workspace_name
-}
